43eb6dc99ccb94b5fd8277ed17f7d086af647103
Swathi_Annamaraju <2400033213@kluniversity.in>
2025-11-30 00:09:18 +0530

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import UserLoginPage from './components/UserLoginPage.jsx';
+import SubscriptionPage from './components/SubscriptionPage.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserLoginPage />
